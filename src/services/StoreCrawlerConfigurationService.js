d368afac5cf2521a2e9146ce5ab32ef78a6441a8
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-06-17 10:05:53 +1200

    Code refactoring

--- src/services/StoreCrawlerConfigurationService.js
+++ src/services/StoreCrawlerConfigurationService.js
@@ -1,73 +1,56 @@
 // @flow
 
 import Immutable from 'immutable';
-import { ParseWrapperService } from 'micro-business-parse-server-common';
+import { ParseWrapperService, Exception } from 'micro-business-parse-server-common';
 import { StoreCrawlerConfiguration } from '../schema';
 import NewSearchResultReceivedEvent from './NewSearchResultReceivedEvent';
 
 export default class StoreCrawlerConfigurationService {
-  static create = info =>
-    new Promise((resolve, reject) => {
-      StoreCrawlerConfiguration.spawn(info).save().then(result => resolve(result.id)).catch(error => reject(error));
-    });
+  static create = async (info) => {
+    const result = await StoreCrawlerConfiguration.spawn(info).save();
 
-  static read = id =>
-    new Promise((resolve, reject) => {
-      ParseWrapperService.createQuery(StoreCrawlerConfiguration)
-        .equalTo('objectId', id)
-        .limit(1)
-        .find()
-        .then((results) => {
-          if (results.length === 0) {
-            reject(`No store crawler configuration found with Id: ${id}`);
-          } else {
-            resolve(new StoreCrawlerConfiguration(results[0]).getInfo());
-          }
-        })
-        .catch(error => reject(error));
-    });
+    return result.id;
+  };
 
-  static update = info =>
-    new Promise((resolve, reject) => {
-      ParseWrapperService.createQuery(StoreCrawlerConfiguration)
-        .equalTo('objectId', info.get('id'))
-        .limit(1)
-        .find()
-        .then((results) => {
-          if (results.length === 0) {
-            reject(`No store crawler configuration found with Id: ${info.get('id')}`);
-          } else {
-            const object = new StoreCrawlerConfiguration(results[0]);
+  static read = async (id) => {
+    const results = await ParseWrapperService.createQuery(StoreCrawlerConfiguration).equalTo('objectId', id).limit(1).find();
 
-            object.updateInfo(info).saveObject().then(() => resolve(object.getId())).catch(error => reject(error));
-          }
-        })
-        .catch(error => reject(error));
-    });
+    if (results.length === 0) {
+      throw new Exception(`No store crawler configuration found with Id: ${id}`);
+    }
 
-  static delete = id =>
-    new Promise((resolve, reject) => {
-      ParseWrapperService.createQuery(StoreCrawlerConfiguration)
-        .equalTo('objectId', id)
-        .limit(1)
-        .find()
-        .then((results) => {
-          if (results.length === 0) {
-            reject(`No store crawler configuration found with Id: ${id}`);
-          } else {
-            results[0].destroy().then(() => resolve()).catch(error => reject(error));
-          }
-        })
-        .catch(error => reject(error));
-    });
+    return new StoreCrawlerConfiguration(results[0]).getInfo();
+  };
 
-  static search = criteria =>
-    new Promise((resolve, reject) =>
-      StoreCrawlerConfigurationService.buildSearchQuery(criteria)
-        .find()
-        .then(results => resolve(Immutable.fromJS(results).map(_ => new StoreCrawlerConfiguration(_).getInfo())))
-        .catch(error => reject(error)),
-    );
+  static update = async (info) => {
+    const results = await ParseWrapperService.createQuery(StoreCrawlerConfiguration).equalTo('objectId', info.get('id')).limit(1).find();
+
+    if (results.length === 0) {
+      throw new Exception(`No store crawler configuration found with Id: ${info.get('id')}`);
+    } else {
+      const object = new StoreCrawlerConfiguration(results[0]);
+
+      await object.updateInfo(info).saveObject();
+
+      return object.getId();
+    }
+  };
+
+  static delete = async (id) => {
+    const results = await ParseWrapperService.createQuery(StoreCrawlerConfiguration).equalTo('objectId', id).limit(1).find();
+
+    if (results.length === 0) {
+      throw new Exception(`No store crawler configuration found with Id: ${id}`);
+    } else {
+      await results[0].destroy();
+    }
+  };
+
+  static search = async (criteria) => {
+    const results = await StoreCrawlerConfigurationService.buildSearchQuery(criteria).find();
+
+    return Immutable.fromJS(results).map(_ => new StoreCrawlerConfiguration(_).getInfo());
+  };
 
   static searchAll = (criteria) => {
     const event = new NewSearchResultReceivedEvent();
@@ -79,10 +62,13 @@ export default class StoreCrawlerConfigurationService {
     };
   };
 
-  static exists = criteria =>
-    new Promise((resolve, reject) =>
-      StoreCrawlerConfigurationService.buildSearchQuery(criteria).count().then(total => resolve(total > 0)).catch(error => reject(error)),
-    );
+  static exists = async (criteria) => {
+    const total = await StoreCrawlerConfigurationService.count(criteria);
+
+    return total > 0;
+  };
+
+  static count = async criteria => StoreCrawlerConfigurationService.buildSearchQuery(criteria).count();
 
   static buildSearchQuery = (criteria) => {
     const query = ParseWrapperService.createQuery(StoreCrawlerConfiguration, criteria);
--- src/services/__tests__/StoreCrawlerConfigurationService.test.js
+++ src/services/__tests__/StoreCrawlerConfigurationService.test.js
@@ -34,268 +34,186 @@ function createCriteriaUsingProvidedStoreCrawlerConfigurationInfo(storeCrawlerCo
 }
 
 describe('create', () => {
-  test('should return the created store crawler configuration Id', (done) => {
-    StoreCrawlerConfigurationService.create(createStoreCrawlerConfigurationInfo())
-      .then((result) => {
-        expect(result).toBeDefined();
-        done();
-      })
-      .catch((error) => {
-        fail(error);
-        done();
-      });
+  test('should return the created store crawler configuration Id', async () => {
+    const result = await StoreCrawlerConfigurationService.create(createStoreCrawlerConfigurationInfo());
+
+    expect(result).toBeDefined();
   });
 
-  test('should create the store crawler configuration', (done) => {
+  test('should create the store crawler configuration', async () => {
     const expectedStoreCrawlerConfigurationInfo = createStoreCrawlerConfigurationInfo();
-    let storeCrawlerConfigurationId;
+    const storeCrawlerConfigurationId = await StoreCrawlerConfigurationService.create(expectedStoreCrawlerConfigurationInfo);
+    const storeCrawlerConfigurationInfo = await StoreCrawlerConfigurationService.read(storeCrawlerConfigurationId);
 
-    StoreCrawlerConfigurationService.create(expectedStoreCrawlerConfigurationInfo)
-      .then((id) => {
-        storeCrawlerConfigurationId = id;
-
-        return StoreCrawlerConfigurationService.read(storeCrawlerConfigurationId);
-      })
-      .then((storeCrawlerConfigurationInfo) => {
-        expectStoreCrawlerConfigurationInfo(storeCrawlerConfigurationInfo, expectedStoreCrawlerConfigurationInfo, storeCrawlerConfigurationId);
-        done();
-      })
-      .catch((error) => {
-        fail(error);
-        done();
-      });
+    expectStoreCrawlerConfigurationInfo(storeCrawlerConfigurationInfo, expectedStoreCrawlerConfigurationInfo, storeCrawlerConfigurationId);
   });
 });
 
 describe('read', () => {
-  test('should reject if the provided store crawler configuration Id does not exist', (done) => {
+  test('should reject if the provided store crawler configuration Id does not exist', async () => {
     const storeCrawlerConfigurationId = uuid();
 
-    StoreCrawlerConfigurationService.read(storeCrawlerConfigurationId).catch((error) => {
-      expect(error).toBe(`No store crawler configuration found with Id: ${storeCrawlerConfigurationId}`);
-      done();
-    });
+    try {
+      await StoreCrawlerConfigurationService.read(storeCrawlerConfigurationId);
+    } catch (ex) {
+      expect(ex.getErrorMessage()).toBe(`No store crawler configuration found with Id: ${storeCrawlerConfigurationId}`);
+    }
   });
 
-  test('should read the existing store crawler configuration', (done) => {
+  test('should read the existing store crawler configuration', async () => {
     const expectedStoreCrawlerConfigurationInfo = createStoreCrawlerConfigurationInfo();
-    let storeCrawlerConfigurationId;
+    const storeCrawlerConfigurationId = await StoreCrawlerConfigurationService.create(expectedStoreCrawlerConfigurationInfo);
+    const storeCrawlerConfigurationInfo = await StoreCrawlerConfigurationService.read(storeCrawlerConfigurationId);
 
-    StoreCrawlerConfigurationService.create(expectedStoreCrawlerConfigurationInfo)
-      .then((id) => {
-        storeCrawlerConfigurationId = id;
-
-        return StoreCrawlerConfigurationService.read(storeCrawlerConfigurationId);
-      })
-      .then((storeCrawlerConfigurationInfo) => {
-        expectStoreCrawlerConfigurationInfo(storeCrawlerConfigurationInfo, expectedStoreCrawlerConfigurationInfo, storeCrawlerConfigurationId);
-        done();
-      })
-      .catch((error) => {
-        fail(error);
-        done();
-      });
+    expectStoreCrawlerConfigurationInfo(storeCrawlerConfigurationInfo, expectedStoreCrawlerConfigurationInfo, storeCrawlerConfigurationId);
   });
 });
 
 describe('update', () => {
-  test('should reject if the provided store crawler configuration Id does not exist', (done) => {
+  test('should reject if the provided store crawler configuration Id does not exist', async () => {
     const storeCrawlerConfigurationId = uuid();
 
-    StoreCrawlerConfigurationService.update(createStoreCrawlerConfigurationInfo().set('id', storeCrawlerConfigurationId)).catch((error) => {
-      expect(error).toBe(`No store crawler configuration found with Id: ${storeCrawlerConfigurationId}`);
-      done();
-    });
+    try {
+      await StoreCrawlerConfigurationService.update(createStoreCrawlerConfigurationInfo().set('id', storeCrawlerConfigurationId));
+    } catch (ex) {
+      expect(ex.getErrorMessage()).toBe(`No store crawler configuration found with Id: ${storeCrawlerConfigurationId}`);
+    }
   });
 
-  test('should return the Id of the updated store crawler configuration', (done) => {
-    let storeCrawlerConfigurationId;
+  test('should return the Id of the updated store crawler configuration', async () => {
+    const storeCrawlerConfigurationId = await StoreCrawlerConfigurationService.create(createStoreCrawlerConfigurationInfo());
+    const id = await StoreCrawlerConfigurationService.update(createStoreCrawlerConfigurationInfo().set('id', storeCrawlerConfigurationId));
 
-    StoreCrawlerConfigurationService.create(createStoreCrawlerConfigurationInfo())
-      .then((id) => {
-        storeCrawlerConfigurationId = id;
-
-        return StoreCrawlerConfigurationService.update(createStoreCrawlerConfigurationInfo().set('id', storeCrawlerConfigurationId));
-      })
-      .then((id) => {
-        expect(id).toBe(storeCrawlerConfigurationId);
-        done();
-      })
-      .catch((error) => {
-        fail(error);
-        done();
-      });
+    expect(id).toBe(storeCrawlerConfigurationId);
   });
 
-  test('should update the existing store crawler configuration', (done) => {
+  test('should update the existing store crawler configuration', async () => {
     const expectedStoreCrawlerConfigurationInfo = createStoreCrawlerConfigurationInfo();
-    let storeCrawlerConfigurationId;
+    const id = await StoreCrawlerConfigurationService.create(createStoreCrawlerConfigurationInfo());
+    const storeCrawlerConfigurationId = await StoreCrawlerConfigurationService.update(expectedStoreCrawlerConfigurationInfo.set('id', id));
+    const storeCrawlerConfigurationInfo = await StoreCrawlerConfigurationService.read(storeCrawlerConfigurationId);
 
-    StoreCrawlerConfigurationService.create(createStoreCrawlerConfigurationInfo())
-      .then(id => StoreCrawlerConfigurationService.update(expectedStoreCrawlerConfigurationInfo.set('id', id)))
-      .then((id) => {
-        storeCrawlerConfigurationId = id;
-
-        return StoreCrawlerConfigurationService.read(storeCrawlerConfigurationId);
-      })
-      .then((storeCrawlerConfigurationInfo) => {
-        expectStoreCrawlerConfigurationInfo(storeCrawlerConfigurationInfo, expectedStoreCrawlerConfigurationInfo, storeCrawlerConfigurationId);
-        done();
-      })
-      .catch((error) => {
-        fail(error);
-        done();
-      });
+    expectStoreCrawlerConfigurationInfo(storeCrawlerConfigurationInfo, expectedStoreCrawlerConfigurationInfo, storeCrawlerConfigurationId);
   });
 });
 
 describe('delete', () => {
-  test('should reject if the provided store crawler configuration Id does not exist', (done) => {
+  test('should reject if the provided store crawler configuration Id does not exist', async () => {
     const storeCrawlerConfigurationId = uuid();
 
-    StoreCrawlerConfigurationService.delete(storeCrawlerConfigurationId).catch((error) => {
-      expect(error).toBe(`No store crawler configuration found with Id: ${storeCrawlerConfigurationId}`);
-      done();
-    });
+    try {
+      await StoreCrawlerConfigurationService.delete(storeCrawlerConfigurationId);
+    } catch (ex) {
+      expect(ex.getErrorMessage()).toBe(`No store crawler configuration found with Id: ${storeCrawlerConfigurationId}`);
+    }
   });
 
-  test('should delete the existing store crawler configuration', (done) => {
-    let storeCrawlerConfigurationId;
+  test('should delete the existing store crawler configuration', async () => {
+    const storeCrawlerConfigurationId = await StoreCrawlerConfigurationService.create(createStoreCrawlerConfigurationInfo());
+    await StoreCrawlerConfigurationService.delete(storeCrawlerConfigurationId);
 
-    StoreCrawlerConfigurationService.create(createStoreCrawlerConfigurationInfo())
-      .then((id) => {
-        storeCrawlerConfigurationId = id;
-        return StoreCrawlerConfigurationService.delete(storeCrawlerConfigurationId);
-      })
-      .then(() => StoreCrawlerConfigurationService.read(storeCrawlerConfigurationId))
-      .catch((error) => {
-        expect(error).toBe(`No store crawler configuration found with Id: ${storeCrawlerConfigurationId}`);
-        done();
-      });
+    try {
+      await StoreCrawlerConfigurationService.read(storeCrawlerConfigurationId);
+    } catch (ex) {
+      expect(ex.getErrorMessage()).toBe(`No store crawler configuration found with Id: ${storeCrawlerConfigurationId}`);
+    }
   });
 });
 
 describe('search', () => {
-  test('should return no store crawler configuration if provided criteria matches no store crawler configuration', (done) => {
-    StoreCrawlerConfigurationService.search(createCriteria())
-      .then((storeCrawlerConfigurations) => {
-        expect(storeCrawlerConfigurations.count()).toBe(0);
-        done();
-      })
-      .catch((error) => {
-        fail(error);
-        done();
-      });
+  test('should return no store crawler configuration if provided criteria matches no store crawler configuration', async () => {
+    const storeCrawlerConfigurationInfos = await StoreCrawlerConfigurationService.search(createCriteria());
+
+    expect(storeCrawlerConfigurationInfos.count()).toBe(0);
   });
 
-  test('should return the store crawler configurations matches the criteria', (done) => {
+  test('should return the storeCrawlerConfigurations matches the criteria', async () => {
     const expectedStoreCrawlerConfigurationInfo = createStoreCrawlerConfigurationInfo();
-    let storeCrawlerConfigurationId;
+    const storeCrawlerConfigurationId = await StoreCrawlerConfigurationService.create(expectedStoreCrawlerConfigurationInfo);
+    const storeCrawlerConfigurationInfos = await StoreCrawlerConfigurationService.search(
+      createCriteriaUsingProvidedStoreCrawlerConfigurationInfo(expectedStoreCrawlerConfigurationInfo),
+    );
 
-    StoreCrawlerConfigurationService.create(expectedStoreCrawlerConfigurationInfo)
-      .then((id) => {
-        storeCrawlerConfigurationId = id;
+    expect(storeCrawlerConfigurationInfos.count()).toBe(1);
 
-        return StoreCrawlerConfigurationService.search(
-          createCriteriaUsingProvidedStoreCrawlerConfigurationInfo(expectedStoreCrawlerConfigurationInfo),
-        );
-      })
-      .then((storeCrawlerConfigurationInfos) => {
-        expect(storeCrawlerConfigurationInfos.count()).toBe(1);
-
-        const storeCrawlerConfigurationInfo = storeCrawlerConfigurationInfos.first();
-        expectStoreCrawlerConfigurationInfo(storeCrawlerConfigurationInfo, expectedStoreCrawlerConfigurationInfo, storeCrawlerConfigurationId);
-        done();
-      })
-      .catch((error) => {
-        fail(error);
-        done();
-      });
+    const storeCrawlerConfigurationInfo = storeCrawlerConfigurationInfos.first();
+    expectStoreCrawlerConfigurationInfo(storeCrawlerConfigurationInfo, expectedStoreCrawlerConfigurationInfo, storeCrawlerConfigurationId);
   });
 });
 
 describe('searchAll', () => {
-  test('should return no store crawler configuration if provided criteria matches no store crawler configuration', (done) => {
+  test('should return no store crawler configuration if provided criteria matches no store crawler configuration', async () => {
     const result = StoreCrawlerConfigurationService.searchAll(createCriteria());
-    let storeCrawlerConfigurations = List();
 
-    result.event.subscribe((storeCrawlerConfiguration) => {
-      storeCrawlerConfigurations = storeCrawlerConfigurations.push(storeCrawlerConfiguration);
-    });
-    result.promise
-      .then(() => {
-        result.event.unsubscribeAll();
-        expect(storeCrawlerConfigurations.count()).toBe(0);
-        done();
-      })
-      .catch((error) => {
-        result.event.unsubscribeAll();
-        fail(error);
-        done();
-      });
+    try {
+      let storeCrawlerConfigurations = List();
+
+      result.event.subscribe(info => (storeCrawlerConfigurations = storeCrawlerConfigurations.push(info)));
+
+      await result.promise;
+      expect(storeCrawlerConfigurations.count()).toBe(0);
+    } finally {
+      result.event.unsubscribeAll();
+    }
   });
 
-  test('should return the store crawler configurations matches the criteria', (done) => {
+  test('should return the storeCrawlerConfigurations matches the criteria', async () => {
     const expectedStoreCrawlerConfigurationInfo = createStoreCrawlerConfigurationInfo();
+    const storeCrawlerConfigurationId1 = await StoreCrawlerConfigurationService.create(expectedStoreCrawlerConfigurationInfo);
+    const storeCrawlerConfigurationId2 = await StoreCrawlerConfigurationService.create(expectedStoreCrawlerConfigurationInfo);
 
-    Promise.all([
-      StoreCrawlerConfigurationService.create(expectedStoreCrawlerConfigurationInfo),
-      StoreCrawlerConfigurationService.create(expectedStoreCrawlerConfigurationInfo),
-    ])
-      .then((ids) => {
-        const storeCrawlerConfigurationIds = List.of(ids[0], ids[1]);
-        const result = StoreCrawlerConfigurationService.searchAll(
-          createCriteriaUsingProvidedStoreCrawlerConfigurationInfo(expectedStoreCrawlerConfigurationInfo),
-        );
-        let storeCrawlerConfigurations = List();
+    const result = StoreCrawlerConfigurationService.searchAll(
+      createCriteriaUsingProvidedStoreCrawlerConfigurationInfo(expectedStoreCrawlerConfigurationInfo),
+    );
+    try {
+      let storeCrawlerConfigurations = List();
 
-        result.event.subscribe((storeCrawlerConfiguration) => {
-          storeCrawlerConfigurations = storeCrawlerConfigurations.push(storeCrawlerConfiguration);
-        });
-        result.promise
-          .then(() => {
-            result.event.unsubscribeAll();
-            expect(storeCrawlerConfigurations.count()).toBe(storeCrawlerConfigurationIds.count());
-            done();
-          })
-          .catch((error) => {
-            result.event.unsubscribeAll();
-            fail(error);
-            done();
-          });
-      })
-      .catch((error) => {
-        fail(error);
-        done();
-      });
+      result.event.subscribe(info => (storeCrawlerConfigurations = storeCrawlerConfigurations.push(info)));
+
+      await result.promise;
+      expect(storeCrawlerConfigurations.count()).toBe(2);
+      expect(storeCrawlerConfigurations.find(_ => _.get('id').localeCompare(storeCrawlerConfigurationId1) === 0)).toBeTruthy();
+      expect(storeCrawlerConfigurations.find(_ => _.get('id').localeCompare(storeCrawlerConfigurationId2) === 0)).toBeTruthy();
+    } finally {
+      result.event.unsubscribeAll();
+    }
   });
 });
 
 describe('exists', () => {
-  test('should return false if no store crawler configuration match provided criteria', (done) => {
-    StoreCrawlerConfigurationService.exists(createCriteria())
-      .then((response) => {
-        expect(response).toBeFalsy();
-        done();
-      })
-      .catch((error) => {
-        fail(error);
-        done();
-      });
+  test('should return false if no store crawler configuration match provided criteria', async () => {
+    const response = await StoreCrawlerConfigurationService.exists(createCriteria());
+
+    expect(response).toBeFalsy();
   });
 
-  test('should return true if any store crawler configuration match provided criteria', (done) => {
+  test('should return true if any store crawler configuration match provided criteria', async () => {
     const storeCrawlerConfigurationInfo = createStoreCrawlerConfigurationInfo();
 
-    StoreCrawlerConfigurationService.create(storeCrawlerConfigurationInfo)
-      .then(() => StoreCrawlerConfigurationService.exists(createCriteriaUsingProvidedStoreCrawlerConfigurationInfo(storeCrawlerConfigurationInfo)))
-      .then((response) => {
-        expect(response).toBeTruthy();
-        done();
-      })
-      .catch((error) => {
-        fail(error);
-        done();
-      });
+    await StoreCrawlerConfigurationService.create(storeCrawlerConfigurationInfo);
+    const response = StoreCrawlerConfigurationService.exists(createCriteriaUsingProvidedStoreCrawlerConfigurationInfo(storeCrawlerConfigurationInfo));
+
+    expect(response).toBeTruthy();
+  });
+});
+
+describe('count', () => {
+  test('should return 0 if no store crawler configuration match provided criteria', async () => {
+    const response = await StoreCrawlerConfigurationService.count(createCriteria());
+
+    expect(response).toBe(0);
+  });
+
+  test('should return the count of store crawler configuration match provided criteria', async () => {
+    const storeCrawlerConfigurationInfo = createStoreCrawlerConfigurationInfo();
+
+    await StoreCrawlerConfigurationService.create(storeCrawlerConfigurationInfo);
+    await StoreCrawlerConfigurationService.create(storeCrawlerConfigurationInfo);
+
+    const response = await StoreCrawlerConfigurationService.count(
+      createCriteriaUsingProvidedStoreCrawlerConfigurationInfo(storeCrawlerConfigurationInfo),
+    );
+
+    expect(response).toBe(2);
   });
 });
