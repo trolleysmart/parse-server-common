970a1e7deeffef913c454b0bbdb3e1a78a7b2722
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-08-21 00:04:10 +1200

    Added ProductPrice schema and unit test

---
+++ src/schema/ProductPrice.js
@@ -0,0 +1,93 @@
+// @flow
+
+import Immutable, { List, Map } from 'immutable';
+import { BaseObject } from 'micro-business-parse-server-common';
+import Store from './Store';
+import Tag from './Tag';
+
+export default class ProductPrice extends BaseObject {
+  static spawn = (info) => {
+    const object = new ProductPrice();
+
+    ProductPrice.updateInfoInternal(object, info);
+
+    return object;
+  };
+
+  static updateInfoInternal = (object, info) => {
+    object.set('name', info.get('name'));
+    object.set('description', info.get('description'));
+    object.set('priceDetails', info.get('priceDetails').toJS());
+    object.set('priceToDisplay', info.get('priceToDisplay'));
+    object.set('saving', info.get('saving'));
+    object.set('savingPercentage', info.get('savingPercentage'));
+    object.set('offerEndDate', info.get('offerEndDate'));
+    object.set('status', info.get('status'));
+
+    if (info.has('storeId')) {
+      const storeId = info.get('storeId');
+
+      if (storeId) {
+        object.set('store', Store.createWithoutData(storeId));
+      }
+    } else if (info.has('store')) {
+      const store = info.get('store');
+
+      if (store) {
+        object.set('store', store);
+      }
+    }
+
+    if (info.has('tagIds')) {
+      const tagIds = info.get('tagIds');
+
+      if (tagIds.isEmpty()) {
+        object.set('tags', []);
+      } else {
+        object.set('tags', tagIds.map(tagId => Tag.createWithoutData(tagId)).toArray());
+      }
+    } else if (info.has('tags')) {
+      const tags = info.get('tags');
+
+      if (tags.isEmpty()) {
+        object.set('tags', []);
+      } else {
+        object.set('tags', tags.toArray());
+      }
+    }
+  };
+
+  constructor(object) {
+    super(object, 'ProductPrice');
+  }
+
+  updateInfo = (info) => {
+    const object = this.getObject();
+
+    ProductPrice.updateInfoInternal(object, info);
+
+    return this;
+  };
+
+  getInfo = () => {
+    const store = new Store(this.getObject().get('store'));
+    const tagObjects = this.getObject().get('tags');
+    const tags = tagObjects ? Immutable.fromJS(tagObjects).map(tag => new Tag(tag).getInfo()) : undefined;
+
+    return Map({
+      id: this.getId(),
+      name: this.getObject().get('name'),
+      description: this.getObject().get('description'),
+      priceDetails: Immutable.fromJS(this.getObject().get('priceDetails')),
+      priceToDisplay: this.getObject().get('priceToDisplay'),
+      saving: this.getObject().get('saving'),
+      savingPercentage: this.getObject().get('savingPercentage'),
+      offerEndDate: this.getObject().get('offerEndDate'),
+      status: this.getObject().get('status'),
+      store: store.getInfo(),
+      storeId: store.getId(),
+      tags,
+      tagIds: tags ? tags.map(tag => tag.get('id')) : List(),
+    });
+  };
+}
---
+++ src/schema/__tests__/ProductPrice.test.js
@@ -0,0 +1,89 @@
+// @flow
+
+import { Map } from 'immutable';
+import uuid from 'uuid/v4';
+import { ProductPrice } from '../';
+import { createStore } from './Store.test';
+
+export function createProductPriceInfo(storeId, tagIds) {
+  return Map({
+    name: uuid(),
+    description: uuid(),
+    priceDetails: Map({
+      price: 10.56,
+    }),
+    priceToDisplay: 12.34,
+    saving: 2.3,
+    savingPercentage: 12,
+    offerEndDate: new Date(),
+    status: 'A',
+    storeId: storeId || createStore().getId(),
+  }).merge(tagIds ? Map({ tagIds }) : Map());
+}
+
+export function createProductPrice(productPriceInfo) {
+  return ProductPrice.spawn(productPriceInfo || createProductPriceInfo());
+}
+
+function expectProductPriceInfo(productPriceInfo, expectedProductPriceInfo) {
+  expect(productPriceInfo.get('name')).toBe(expectedProductPriceInfo.get('name'));
+  expect(productPriceInfo.get('description')).toBe(expectedProductPriceInfo.get('description'));
+  expect(productPriceInfo.get('priceDetails')).toEqual(expectedProductPriceInfo.get('priceDetails'));
+  expect(productPriceInfo.get('priceToDisplay')).toEqual(expectedProductPriceInfo.get('priceToDisplay'));
+  expect(productPriceInfo.get('saving')).toEqual(expectedProductPriceInfo.get('saving'));
+  expect(productPriceInfo.get('savingPercentage')).toEqual(expectedProductPriceInfo.get('savingPercentage'));
+  expect(productPriceInfo.get('offerEndDate')).toEqual(expectedProductPriceInfo.get('offerEndDate'));
+  expect(productPriceInfo.get('status')).toEqual(expectedProductPriceInfo.get('status'));
+  expect(productPriceInfo.get('storeId')).toBe(expectedProductPriceInfo.get('storeId'));
+  expect(productPriceInfo.get('tags')).toEqual(expectedProductPriceInfo.get('tags'));
+}
+
+describe('constructor', () => {
+  test('should set class name', () => {
+    expect(createProductPrice().className).toBe('ProductPrice');
+  });
+});
+
+describe('static public methods', () => {
+  test('spawn should set provided info', () => {
+    const productPriceInfo = createProductPriceInfo();
+    const object = createProductPrice(productPriceInfo);
+    const info = object.getInfo();
+
+    expectProductPriceInfo(info, productPriceInfo);
+  });
+});
+
+describe('public methods', () => {
+  test('getObject should return provided object', () => {
+    const object = createProductPrice();
+
+    expect(new ProductPrice(object).getObject()).toBe(object);
+  });
+
+  test('getId should return provided object Id', () => {
+    const object = createProductPrice();
+
+    expect(new ProductPrice(object).getId()).toBe(object.id);
+  });
+
+  test('updateInfo should update object info', () => {
+    const object = createProductPrice();
+    const updatedProductPriceInfo = createProductPriceInfo();
+
+    object.updateInfo(updatedProductPriceInfo);
+
+    const info = object.getInfo();
+
+    expectProductPriceInfo(info, updatedProductPriceInfo);
+  });
+
+  test('getInfo should return provided info', () => {
+    const productPriceInfo = createProductPriceInfo();
+    const object = createProductPrice(productPriceInfo);
+    const info = object.getInfo();
+
+    expect(info.get('id')).toBe(object.getId());
+    expectProductPriceInfo(info, productPriceInfo);
+  });
+});
